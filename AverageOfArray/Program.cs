d93fbfbfa91dfe1c29378c8c06f94cced5c1a84b
James Luncan <jluncan@tql.com>
2024-06-12 15:24:32 -0400

    initial commit

---
+++ AverageOfArray/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AverageOfArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = { 97, 92, 87, 88, 78 }; 
+
+            int sum = 0;
+
+            sum = sum + scores[0];
+            sum =sum + scores[1];
+            sum = sum + scores[2];
+            sum = sum + scores[3];
+            sum = sum + scores[4];
+
+            Console.WriteLine($"The sum is {sum}");
+            
+            
+            
+
+            decimal avg = 0;
+
+            avg = sum / (decimal)scores.Length;
+
+            Console.WriteLine($"The average is {avg}"); 
+        }
+    }
+}
